9a7bf9be8893ddb3f0bd4c46ed1b352e71b9f69e
Syzygianinfern0 <spsharan2000@gmail.com>
2020-08-03 19:42:53 +0530

    constructors and destructors

--- commitizer/terminal.py
+++ commitizer/terminal.py
@@ -1,9 +1,20 @@
-import os
+from git import Repo
 
 
 class Terminal:
     def __init__(self, branch='gh-pages'):
-        self.execute(f'git checkout {branch}')
+        self.repo = Repo(search_parent_directories=True)
+        self.tests()
+
+        self.original_branch = self.repo.active_branch
+
+        self.repo.git.stash()
+        self.repo.git.checkout(branch)
+
+    def tests(self):
+        if self.repo.bare:
+            print('Init repo?')
+            self.repo.init('.')
 
     @staticmethod
     def execute(command: str):
@@ -15,4 +26,8 @@ class Terminal:
 
         """
 
-        os.system(command)
+        # os.system(command)
+
+    def __del__(self):
+        self.repo.git.checkout(self.original_branch)
+        self.repo.git.pop('Terminal Constructor')
