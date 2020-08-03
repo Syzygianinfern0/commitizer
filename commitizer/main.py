70f2d4dadf57fc91ee57b281e7036f75fac9daa6
S P Sharan <spsharan2000@gmail.com>
2020-08-03 19:02:40 +0530

    Rudimentary Script for Commits (#8)
    * Package init
    
    * Execute terminal cmds
    
    * Basic Terminal Class
    
    * fake commit
    
    * fake commit
    
    * fake commit
    
    * fake commit
    
    * fake commit
    
    * Fake commits looop
    
    * requirements.txt initialize

---
+++ commitizer/main.py
@@ -0,0 +1,8 @@
+from commitizer.terminal import Terminal
+
+COMMIT_CMD = 'git commit --date="{}" --allow-empty -m "{}" '
+
+if __name__ == '__main__':
+    terminal_instance = Terminal()
+    for _ in range(5):
+        terminal_instance.execute(COMMIT_CMD.format('10 years ago', 'fake commit'))
