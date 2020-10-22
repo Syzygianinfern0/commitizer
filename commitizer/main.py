27c16eba763dbc10ad079183b7a10f47300f76ba
Syzygianinfern0 <spsharan2000@gmail.com>
2020-10-22 08:22:06 +0530

    Reorder these lines

--- commitizer/main.py
+++ commitizer/main.py
@@ -5,8 +5,8 @@ from looper import dates_between
 
 def main():
     for date in dates_between():
-        os.system(f'git commit --date="{date}" --allow-empty -m "Fake Commit"')
         print(f'git commit --date="{date}" --allow-empty -m "Fake Commit"')
+        os.system(f'git commit --date="{date}" --allow-empty -m "Fake Commit"')
 
 
 if __name__ == '__main__':
