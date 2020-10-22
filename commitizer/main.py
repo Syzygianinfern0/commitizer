fa7f6dd21a3c818ef7b9267932fc2f01c459056a
Syzygianinfern0 <spsharan2000@gmail.com>
2020-10-22 07:46:46 +0530

    Legacy code

--- commitizer/main.py
+++
@@ -1,8 +0,0 @@
-from terminal import Terminal
-
-COMMIT_CMD = 'git commit --date="{}" --allow-empty -m "{}" '
-
-if __name__ == '__main__':
-    terminal_instance = Terminal()
-    # for _ in range(5):
-    #     terminal_instance.execute(COMMIT_CMD.format('10 years ago', 'fake commit'))
