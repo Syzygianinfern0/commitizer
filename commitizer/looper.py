51d359083ba72130c1788f684cb038896708a1d5
Syzygianinfern0 <spsharan2000@gmail.com>
2020-10-22 08:10:38 +0530

    Yield date As a generator

--- commitizer/looper.py
+++ commitizer/looper.py
@@ -2,13 +2,16 @@ from datetime import date
 from datetime import timedelta
 
 
-def dateiter(sdate=date(2008, 8, 15), edate=date(2008, 9, 15)):
-    delta = edate - sdate  # as timedelta
+def dates_between(start_date=date(2008, 8, 15), end_date=date(2008, 9, 15)):
+    # date(yyyy, (m)m, (d)d)
+    delta = end_date - start_date  # as timedelta
 
     for i in range(delta.days + 1):
-        day = sdate + timedelta(days=i)
-        print(day)
+        day = start_date + timedelta(days=i)
+        # print(day)
+        yield day
 
 
 if __name__ == '__main__':
-    dateiter()
+    for date in dates_between():
+        print(date)
