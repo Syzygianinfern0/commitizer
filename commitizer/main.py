885c37a3e68827fd5d391f35482e8b9eb4527730
Syzygianinfern0 <spsharan2000@gmail.com>
2020-10-22 08:58:13 +0530

    Random between two numbers generator

--- commitizer/main.py
+++ commitizer/main.py
@@ -1,6 +1,6 @@
 import argparse
 import datetime
-import os
+import random
 
 from looper import dates_between
 
@@ -8,9 +8,16 @@ from looper import dates_between
 # noinspection PyShadowingNames
 def main(start_date, end_date, number):
     for date in dates_between(start_date, end_date):
-        for _ in range(int(number)):
+
+        try:
+            count = int(number)
+        except ValueError:
+            number_split = number.split(',')
+            count = random.randint(int(number_split[0]), int(number_split[1]))
+
+        for _ in range(count):
             print(f'git commit --date="{date}" --allow-empty -m "Fake Commit"')
-            os.system(f'git commit --date="{date}" --allow-empty -m "Fake Commit"')
+            # os.system(f'git commit --date="{date}" --allow-empty -m "Fake Commit"')
 
 
 def str2date(str_date):
@@ -21,7 +28,7 @@ if __name__ == '__main__':
     my_parser = argparse.ArgumentParser()
     my_parser.add_argument('-s', '--start-date', type=str, default='18/08/2008')
     my_parser.add_argument('-e', '--end-date', type=str, default='20/08/2008')
-    my_parser.add_argument('-n', '--number', type=str, default='3')
+    my_parser.add_argument('-n', '--number', type=str, default='1,3')
 
     args = my_parser.parse_args()
 
