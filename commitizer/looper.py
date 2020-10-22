e4a580962d8108c4547248576ebc45d8ac80666c
S P Sharan <spsharan2000@gmail.com>
2020-10-22 08:23:19 +0530

    Date Looper for main script (#12)
    * Looping script up in place
    
    * Yield date
    As a generator
    
    * Main script now loops
    
    * Sources root pycharm fixed
    
    * Reorder these lines

---
+++ commitizer/looper.py
@@ -0,0 +1,17 @@
+from datetime import date
+from datetime import timedelta
+
+
+def dates_between(start_date=date(2008, 8, 15), end_date=date(2008, 8, 17)):
+    # date(yyyy, (m)m, (d)d)
+    delta = end_date - start_date  # as timedelta
+
+    for i in range(delta.days + 1):
+        day = start_date + timedelta(days=i)
+        # print(day)
+        yield day
+
+
+if __name__ == '__main__':
+    for date in dates_between():
+        print(date)
--- commitizer/main.py
+++ commitizer/main.py
@@ -1,8 +1,12 @@
 import os
 
+from looper import dates_between
+
 
 def main():
-    os.system('git commit --date="22/06/2012" --allow-empty -m "Fake Commit"')
+    for date in dates_between():
+        print(f'git commit --date="{date}" --allow-empty -m "Fake Commit"')
+        os.system(f'git commit --date="{date}" --allow-empty -m "Fake Commit"')
 
 
 if __name__ == '__main__':
