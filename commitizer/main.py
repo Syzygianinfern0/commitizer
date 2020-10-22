ba846748aeb9f8eeb8659194fa045fcb34bdb80f
Syzygianinfern0 <spsharan2000@gmail.com>
2020-10-22 08:45:23 +0530

    Args are now parsed

--- commitizer/main.py
+++ commitizer/main.py
@@ -1,13 +1,26 @@
+import argparse
+import datetime
 import os
 
 from looper import dates_between
 
 
-def main():
-    for date in dates_between():
-        print(f'git commit --date="{date}" --allow-empty -m "Fake Commit"')
-        os.system(f'git commit --date="{date}" --allow-empty -m "Fake Commit"')
+# noinspection PyShadowingNames
+def main(start_date, end_date, number):
+    for date in dates_between(start_date, end_date):
+        for _ in range(int(number)):
+            print(f'git commit --date="{date}" --allow-empty -m "Fake Commit"')
+            os.system(f'git commit --date="{date}" --allow-empty -m "Fake Commit"')
 
 
 if __name__ == '__main__':
-    main()
+    my_parser = argparse.ArgumentParser()
+    my_parser.add_argument('-s', '--start-date', type=str, default='18/08/2008')
+    my_parser.add_argument('-e', '--end-date', type=str, default='20/08/2008')
+    my_parser.add_argument('-n', '--number', type=str, default='3')
+
+    args = my_parser.parse_args()
+
+    start_date = datetime.datetime.strptime(args.start_date, '%d/%m/%Y')
+    end_date = datetime.datetime.strptime(args.end_date, '%d/%m/%Y')
+    main(start_date, end_date, args.number)
